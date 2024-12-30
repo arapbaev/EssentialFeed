a5ffcec15fdea7e4da7e8a4f7fd139a86a3b5603
Aslan Arapbaev <arapbaev@gmail.com>
2024-12-30 23:39:06 +0600

    Extract FailableRetrieveFeedStoreSpecs checks into reusable assertions

--- EssentialFeed/EssentialFeedTests/Feed Cache/FeedStoreSpecs.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/FeedStoreSpecs.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public protocol FeedStoreSpecs {
+protocol FeedStoreSpecs {
     func test_retrieve_deliversEmptyOnEmptyCache()
     func test_retrieve_hasNoSideEffectsOnEmptyCache()
     func test_retrieve_deliversFoundValuesOnNonEmptyCache()
@@ -20,24 +20,24 @@ public protocol FeedStoreSpecs {
     func test_delete_deliversNoErrorOnEmptyCache()
     func test_delete_hasNoSideEffectOnEmptyCache()
     func test_delete_deliversNoErrorOnNonEmptyCache()
-    func test_delete_emptiesPreviouslyInsertedCache()  
+    func test_delete_emptiesPreviouslyInsertedCache()
     
     func test_storeSideEffects_runSerially()
 }
 
-public protocol FailableRetrieveFeedStoreSpecs: FeedStoreSpecs {
+protocol FailableRetrieveFeedStoreSpecs: FeedStoreSpecs {
     func test_retrieve_deliversFailureOnRetrievalError()
     func test_retrieve_hasNoSideEffectsOnFailure()
 }
 
-public protocol FailableInsertFeedStoreSpecs: FeedStoreSpecs{
+protocol FailableInsertFeedStoreSpecs: FeedStoreSpecs{
     func test_insert_deliversErrorOnInsertionError()
     func test_insert_hasNoSideEffectsOnInsertionError()
 }
 
-public protocol FailableDeleteFeedStoreSpecs: FeedStoreSpecs {
+protocol FailableDeleteFeedStoreSpecs: FeedStoreSpecs {
     func test_delete_deliversErrorOnDeletionError()
     func test_delete_hasNoSideEffectsOnDeletionError()
 }
 
-public typealias FailableFeedStoreSpecs = FailableRetrieveFeedStoreSpecs & FailableInsertFeedStoreSpecs & FailableDeleteFeedStoreSpecs
+typealias FailableFeedStoreSpecs = FailableRetrieveFeedStoreSpecs & FailableInsertFeedStoreSpecs & FailableDeleteFeedStoreSpecs
